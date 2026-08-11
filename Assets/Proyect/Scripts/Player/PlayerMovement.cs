6394fc5f4ebb9682fa7223c489ef421bdee8a321
Sofia Vanegas Cordoba <sofia.vanegasc@udea.edu.co>
2026-08-11 18:08:19 -0500

    Feature: Player Movement n attack prototype

---
+++ Assets/Proyect/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private InputSystem inputActions;
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        inputActions ??= new InputSystem();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    void Update()
+    {
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+        transform.position += direction * (moveSpeed * Time.deltaTime);
+    }
+}
