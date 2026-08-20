a6326e1e4d599d3fcce9570eff155c9455b9e395
NairbHub <banp02nairb@gmail.com>
2026-08-20 17:37:10 -0500

    UI Integration
    Buttons
    HoverScript
    MenuScript 2.0
    UI Art Integrated

--- Assets/Project/Scripts/UI/Menu.cs
+++ Assets/Project/Scripts/UI/Menu.cs
@@ -3,23 +3,46 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [Header("Main Menu Buttons")]
     [SerializeField] private Button startGameButton;
     [SerializeField] private Button howToPlayButton;
     [SerializeField] private Button optionsButton;
     [SerializeField] private Button creditsButton;
 
+    [Header("Panels")]
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject howToPlayPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+    [Header("Back Buttons")]
+    [SerializeField] private Button howToPlayBackButton;
+    [SerializeField] private Button optionsBackButton;
+    [SerializeField] private Button creditsBackButton;
+
     private void Start()
     {
+        // Main Menu
         startGameButton.onClick.AddListener(StartGame);
         howToPlayButton.onClick.AddListener(HowToPlay);
         optionsButton.onClick.AddListener(Options);
         creditsButton.onClick.AddListener(Credits);
+
+        // Back buttons
+        howToPlayBackButton.onClick.AddListener(BackToMainMenu);
+        optionsBackButton.onClick.AddListener(BackToMainMenu);
+        creditsBackButton.onClick.AddListener(BackToMainMenu);
+
+        // Estado inicial
+        ShowMainMenu();
     }
 
     private void StartGame()
     {
         print("StartGame");
+
         StartCoroutine(Fade.Instance.FadeOut());
+
         // Logica para obtener el ultimo nivel completado
         // FlowManager.Instance.GoToScene("");
     }
@@ -27,18 +50,42 @@ public class Menu : MonoBehaviour
     private void HowToPlay()
     {
         print("HowToPlay");
-        //TODO: Canvas
+        ShowPanel(howToPlayPanel);
     }
 
     private void Options()
     {
         print("Options");
-        //TODO: Canvas
+        ShowPanel(optionsPanel);
     }
 
     private void Credits()
     {
         print("Credits");
-        //TODO: Canvas
+        ShowPanel(creditsPanel);
+    }
+
+    private void BackToMainMenu()
+    {
+        print("Back to Main Menu");
+        ShowMainMenu();
+    }
+
+    private void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        howToPlayPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    private void ShowPanel(GameObject panelToShow)
+    {
+        mainMenuPanel.SetActive(false);
+        howToPlayPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+
+        panelToShow.SetActive(true);
     }
 }
