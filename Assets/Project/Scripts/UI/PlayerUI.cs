4271d40c6c723d17621a997cdefb103c57f2866c
Sofia Vanegas Cordoba <sofia.vanegasc@udea.edu.co>
2026-08-23 01:10:50 -0500

    Better Player UI

--- Assets/Project/Scripts/UI/PlayerUI.cs
+++ Assets/Project/Scripts/UI/PlayerUI.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private BombThrower bombThrower;
     [SerializeField] private TextMeshProUGUI attemptsText;
     [SerializeField] private TextMeshProUGUI cooldownText;
+    private Image iconImage;
+
+    private GameObject parentObject;
 
     void Start()
     {
@@ -14,6 +18,8 @@ public class PlayerUI : MonoBehaviour
         bombThrower.CooldownLoggerEvent += UpdateCooldownUI;
         attemptsText.text = playerStats.CurrentAttempts.ToString();
         cooldownText.text = "Q";
+        parentObject = cooldownText.gameObject.transform.parent.gameObject;
+        iconImage = parentObject.GetComponentInChildren<Image>();
     }
 
     private void UpdateAttemptsUI()
@@ -23,18 +29,21 @@ public class PlayerUI : MonoBehaviour
 
     private void UpdateCooldownUI()
     {
+        print("Current charges: " + bombThrower.CurrentCharges);
         if (bombThrower.CurrentCharges == 0)
         {
-            cooldownText.gameObject.transform.parent.gameObject.SetActive(false);
+            parentObject.SetActive(false);
             return;
         }
-        cooldownText.gameObject.transform.parent.gameObject.SetActive(true);
+        parentObject.SetActive(true);
         if (bombThrower.CooldownTimer <= 0)
         {
             cooldownText.text = "Q";
+            iconImage.color = new Color(1f, 1f, 1f, 1f); // Set the icon color to white
             return;
         }
         cooldownText.text = bombThrower.CooldownTimer.ToString("F0");
+        iconImage.color = new Color(0f, 0f, 0f, 0.5f); // Set the icon color to semi-transparent black
     }
 
     public void SetActive(bool isActive)
